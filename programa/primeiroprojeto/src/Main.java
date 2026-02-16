ee4d9c9680623c0e11be8436cfd0f2c4b7bff7ba
Fabricio Guimaraes de Lima <fabricio@lopesecia.com.br>
2026-02-16 17:59:46 -0300

    Aula 02 em desenvolvimento

--- programa/primeiroprojeto/src/Main.java
+++ programa/primeiroprojeto/src/Main.java
@@ -7,6 +7,35 @@ public class Main{
     System.out.println("Ano de lançamento: " + anoDeLancamento);
     boolean incluidoNoPlano = true;
     double notaDoFilme = 8.1;
+    double media = (9.8 + 6.3 + 8.0) / 3;
+    System.out.println(media);
+
+    String sinopse;
+    sinopse = "Filme de aventura com galã dos anos 80";
+    System.out.println(sinopse);
+
+    //Text Blocks
+    sinopse = """
+            Filme 1
+            Filme 2
+            Filme 3
+            """;
+    System.out.println(sinopse);
+
+    String mensagem = String.format("Meu nome é %s, minha idade é %d, tenho %.2f de altura", "Fabricio", 52, 1.73);
+    System.out.println(mensagem);
+
+
+    mensagem = String.format("""
+            Meu nome é %s, minha idade é %d, tenho %.2f de altura        
+            """, "Fabricio", 52, 1.73);
+    System.out.println(mensagem);
+
+    mensagem = """
+            Meu nome é %s, minha idade é %d, tenho %.2f de altura        
+            """.formatted("Fabricio", 52, 1.735);
+    System.out.println(mensagem);
+
   }
 
 }
