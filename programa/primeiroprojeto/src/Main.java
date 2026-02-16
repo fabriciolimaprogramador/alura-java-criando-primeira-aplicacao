30d4570c846fd04b3ad5ef75b09659da8c636cc1
Fabricio Guimaraes de Lima <fabricio@lopesecia.com.br>
2026-02-16 14:03:10 -0300

    Aula 02 em desenvolvimento

--- programa/primeiroprojeto/src/Main.java
+++ programa/primeiroprojeto/src/Main.java
@@ -1,26 +1,12 @@
-//Versão 25 do java
-//void main() {
-//  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-//  // to see how IntelliJ IDEA suggests fixing it.
-//  IO.println(String.format("Hello and welcome!"));
-//
-//  for (int i = 1; i <= 5; i++) {
-//    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//    IO.println("i = " + i);
-//  }
-//}
+public class Main{
 
-//Versão anterior a 25 do java
-public static void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  System.out.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    System.out.println("i = " + i);
+  public static void main() {
+    System.out.println("Esse é o Screen Match");
+    System.out.println("Filme: Top Gun: Maverick");
+    int anoDeLancamento = 2022;
+    System.out.println("Ano de lançamento: " + anoDeLancamento);
+    boolean incluidoNoPlano = true;
+    double notaDoFilme = 8.1;
   }
-}
 
+}
